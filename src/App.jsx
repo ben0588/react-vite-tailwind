b35477a80d51c0a9783661da20ef7958fa6d0dd8
ben <energy95272z@gmail.com>
2023-09-21 12:03:53 +0800

    git init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+      <h2 className="bg-clip">Test</h2>
+    </>
+  );
+}
+
+export default App;
